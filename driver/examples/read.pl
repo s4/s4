b270b5c7545e7286fe50ff70e9741d4eaa4ce788
Anand Kesari <ak@s4.io>
2011-02-01 14:45:16 -0800

    Connection mode as command line arg.

--- driver/examples/read.pl
+++ driver/examples/read.pl
@@ -1,8 +1,7 @@
 use IO::S4::Client;
 use Data::Dumper;
 
-$rm = $ARGV[0];
-$wm = $ARGV[1];
+my $mode = eval $ARGV[0];
 
 $c = new IO::S4::Client("localhost", 2334);
 
@@ -13,10 +12,6 @@ $c->init() or die "Failed to initialize.";
 print "--------------------------------------------------------------------------------\n";
 print "Initialized: " . Dumper($c);
 print "--------------------------------------------------------------------------------\n";
-
-my $mode = {};
-$mode->{'readMode'} = $rm if defined $rm;
-$mode->{'writeMode'} = $wm if defined $wm;
 
 print Dumper($rm, $wm, $mode);
 $c->connect($mode) or die "Failed to connect.";
