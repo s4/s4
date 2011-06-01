e43eeb3ee0473ab223a1d0144032a02c0f74e1c1
Bruce Robbins <robbins@everychoose-lm.corp.yahoo.com>
2011-06-01 15:29:44 -0700

    Fix source file notice

--- s4-tools/loadgenerator/src/main/java/io/s4/tools/loadgenerator/LoadGenerator.java
+++ s4-tools/loadgenerator/src/main/java/io/s4/tools/loadgenerator/LoadGenerator.java
@@ -1,11 +1,10 @@
-package io.s4.tools.loadgenerator;
 /*
  * Copyright (c) 2010 Yahoo! Inc. All rights reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 	        http://www.apache.org/licenses/LICENSE-2.0
+ *          http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -14,7 +13,7 @@ package io.s4.tools.loadgenerator;
  * language governing permissions and limitations under the
  * License. See accompanying LICENSE file. 
  */
-
+package io.s4.tools.loadgenerator;
 
 import io.s4.client.Driver;
 import io.s4.client.Message;
