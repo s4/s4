27310bbba535e14512e69c13b1f2f8cfd20dd6af
Bruce Robbins <robbins@everychoose-lm.corp.yahoo.com>
2011-07-18 10:39:45 -0700

    inject.sh would not run on Linux when current directory was not the script directory; fix that

--- s4-driver/examples/src/main/resources/scripts/inject.sh
+++ s4-driver/examples/src/main/resources/scripts/inject.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+osx=false
+case "`uname`" in
+Darwin*) osx=true;;
+esac
+
+if $osx; then
+    READLINK="stat"    
+else
+    READLINK="readlink"
+fi
+
+BASE_DIR=`dirname $($READLINK -f $0)`
+INJECT_HOME=`$READLINK -f ${BASE_DIR}/..`
+
 CP_SEP=":"
-CLASSPATH=`find ../ -name "*.jar" | awk '{p=$0"'$CP_SEP'"p;} END {print p}'`
+CLASSPATH=`find ${INJECT_HOME} -name "*.jar" | awk '{p=$0"'$CP_SEP'"p;} END {print p}'`
 
 JAVA_LOC=""
 if [ "x$JAVA_HOME" != "x" ] ; then
@@ -7,6 +23,6 @@ if [ "x$JAVA_HOME" != "x" ] ; then
 fi
 
 CMD="${JAVA_LOC}java -classpath $CLASSPATH io.s4.client.example.Inject $1 $2 $3 $4"
-echo $CMD
+#echo $CMD
 $CMD
 
